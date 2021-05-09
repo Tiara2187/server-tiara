63b0cfdc76bc09c90e96185b4450248a2009fa29
tiara2187 <tiaramuttianingtyas23@gmail.com>
2021-05-09 17:14:28 +0700

    update config

--- config/connect.js
+++ config/connect.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose")
 
 module.exports = () => {
-    mongoose.connect(process.env.DBHOST, {
+    const finalserver = 'mongodb+srv://Tiaramuttianingtyas23:tiara2398@cluster0.yhwyg.mongodb.net/tokotiara?authSource=admin&replicaSet=atlas-3np62b-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true'
+    mongoose.connect(process.env.MONGODB_URI || finalserver, {
         useUnifiedTopology: true,
         useFindAndModify: false,
         useNewUrlParser: true,
@@ -9,7 +10,7 @@ module.exports = () => {
     });
 
     const db = mongoose.connection;
-    db.on("error", console.error.bind("connection error"));
+    db.on("error", console.error.bind("connection error", finalserver));
     db.once("open", () => {
         console.log("connection OK");
     });
--- config/connect.ts
+++ config/connect.ts
@@ -4,14 +4,16 @@ class mongoDB {
   constructor() {
     this.connect()
   }
+ 
     public connect():void{
-        mongoose.connect(process.env.DBHOST, {
+      const finalserver = 'mongodb+srv://Tiaramuttianingtyas23:tiara2398@cluster0.yhwyg.mongodb.net/tokotiara?authSource=admin&replicaSet=atlas-3np62b-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true'
+        mongoose.connect(process.env.MONGODB_URI || finalserver, {
           useUnifiedTopology: true,
           useFindAndModify: false,
           useNewUrlParser: true,
           useCreateIndex: true,
         }, ()=>{
-          console.log("connected to database");
+          console.log("connected to database", finalserver);
         })
       }
     }
